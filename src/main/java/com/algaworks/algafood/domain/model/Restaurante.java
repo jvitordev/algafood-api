d8567fa1194a8780f754bf78d41e812ef1dde1d1
Joao Vitor <joaovitorti07@gmail.com>
2022-11-07 21:26:58 -0300

    Aula 3.5. Criando as tabelas do banco a partir das entidades

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -6,7 +6,6 @@ import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
 public class Restaurante {
