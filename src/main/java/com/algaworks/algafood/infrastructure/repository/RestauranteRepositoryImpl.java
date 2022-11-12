410d0991b181611eb49e4abf144826e6d738b897
Joao Vitor <joaovitorti07@gmail.com>
2022-11-12 18:36:34 -0300

    Aula 3.17. Mapeando relacionamento com @ManyToOne

--- src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -5,12 +5,14 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 
-public class RestauranteRepositoryImpl implements RestauranteRepository{
+@Component
+public class RestauranteRepositoryImpl implements RestauranteRepository {
 
 	@PersistenceContext
 	private EntityManager manager;
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,7 @@
-insert into cozinha (nome) values ('Tailandesa');
-insert into cozinha (nome) values ('Indiana');
+INSERT INTO cozinha (id, nome) VALUES (1, 'Tailandesa');
+INSERT INTO cozinha (id, nome) VALUES (2, 'Indiana');
+
+INSERT INTO restaurante (nome, taxa_frete, cozinha_id) VALUES ('Acopiara', 2, 1);
+INSERT INTO restaurante (nome, taxa_frete, cozinha_id) VALUES ('Iguatu', 1.5, 1);
+INSERT INTO restaurante (nome, taxa_frete, cozinha_id) VALUES ('Icó', 3, 2);
+
