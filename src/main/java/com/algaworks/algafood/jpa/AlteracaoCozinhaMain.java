3ace3daf6f29e47d1175c7efff7cc39059d5e906
Joao Vitor <joaovitorti07@gmail.com>
2022-11-12 10:56:03 -0300

    Aula 3.14. Conhecendo e implementando o padrão Repository

--- src/main/java/com/algaworks/algafood/jpa/AlteracaoCozinhaMain.java
+++ src/main/java/com/algaworks/algafood/jpa/AlteracaoCozinhaMain.java
@@ -1,13 +1,12 @@
 package com.algaworks.algafood.jpa;
 
-import java.util.List;
-
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
 
 import com.algaworks.algafood.AlgafoodApiApplication;
 import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
 public class AlteracaoCozinhaMain {
 
@@ -17,13 +16,13 @@ public class AlteracaoCozinhaMain {
 				.web(WebApplicationType.NONE)
 				.run(args);
 		
-		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+		CozinhaRepository cozinhas = applicationContext.getBean(CozinhaRepository.class);
 		
 		Cozinha cozinha = new Cozinha();
 		cozinha.setId(1L);
 		cozinha.setNome("Brasileira");
 		
-		cadastroCozinha.salvar(cozinha);
+		cozinhas.adicionar(cozinha);
 	}
 
 }
