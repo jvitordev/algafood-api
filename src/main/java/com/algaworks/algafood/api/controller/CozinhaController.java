3924042916f478a2192562b0d5d70967b6820a17
Joao Vitor <joaovitorti07@gmail.com>
2022-11-17 12:48:33 -0300

    Aula 4.21. Corrigindo o Status HTTP para resource inexistente

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,8 +3,6 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -37,18 +35,10 @@ public class CozinhaController {
     public ResponseEntity<Cozinha> porId(@PathVariable(value = "cozinhaId", required = false) Long id) {
         Cozinha cozinha =  cozinhaRepository.porId(id);
 
+        if (cozinha == null) {
+            return ResponseEntity.notFound().build();
+        }
 
-        // Editando o status e o corpo da resposta HTTP
-
-        // return ResponseEntity.status(HttpStatus.OK).build(); // STATUS + CORPO VAZIO
-        // return ResponseEntity.status(HttpStatus.OK).body(cozinha); // STATUS + CORPO
-        // return ResponseEntity.ok(cozinha); // ATALHO PARA STATUS + CORPO
-
-        // Editando o Header da resposta HTTP
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.LOCATION, "http://api.algafood.local:8080/cozinhas");
-
-        return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
+        return ResponseEntity.ok(cozinha);
     }
 }
