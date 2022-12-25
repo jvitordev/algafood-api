42ef43506520e660bb593b148f15b6cea4e9bccc
Joao Vitor <joaovitorti07@gmail.com>
2022-12-25 14:44:24 -0300

    Aula 8.21. Tratando a exception InvalidFormatException na desserialização

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 public enum ProblemType {
 
     
-    MENSAGEM_INCONPREENSIVEL("Mensagem inconpreensível", "/mensagem-incompreensivel"),
+    MENSAGEM_INCOMPREENSIVEL("Mensagem inconpreensível", "/mensagem-incompreensivel"),
     ENTIDADE_NAO_ENCONTRADA("Entidade não encontrada", "/entidade-nao-encontada"),
     ENTIDADE_EM_USO("Entidade em uso", "/entidade-em-uso"),
     ERRO_NEGOCIO("Violação de regra de negócio", "/erro-negocio");
