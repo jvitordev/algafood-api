0aeffc5b97202e28a0e6c88659207249950018d3
Joao Vitor <joaovitorti07@gmail.com>
2023-02-27 15:39:08 -0300

    Aula 10.9. Validando o corpo da resposta HTTP

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -15,11 +15,12 @@
 package com.algaworks.algafood;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
@@ -42,6 +43,7 @@ public class CadastroCozinhaIT {
 		.when()
 			.get()
 		.then()
-			.statusCode(HttpStatus.OK.value());
+			.body("", hasSize(4))
+			.body("nome", hasItems("Indiana", "Tailandesa"));
 	}
 }
