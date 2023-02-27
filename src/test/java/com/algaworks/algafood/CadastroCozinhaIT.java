d5c447f5acddd21b8360c468b364ea3764382b0b
Joao Vitor <joaovitorti07@gmail.com>
2023-02-27 17:31:08 -0300

    Aula 10.10. Criando um método para fazer setup dos testes

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -18,9 +18,11 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.hasSize;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
@@ -31,14 +33,29 @@ public class CadastroCozinhaIT {
 	@LocalServerPort
 	private int port;
 
+	@BeforeEach
+	public void setUp() {
+
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		RestAssured.port = port;
+		RestAssured.basePath = "/cozinhas";
+	}
+
 	@Test
 	public void deveRetornarStatus200_QuandoConsultarCozinhas() {
 
-		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-		
 		given()
-			.basePath("/cozinhas")
-			.port(port)
+			.accept(ContentType.JSON)
+		.when()
+			.get()
+		.then()
+			.statusCode(HttpStatus.OK.value());
+	}
+
+	@Test
+	public void deveConter4Cozinhas_QuandoConsultarCozinhas() {
+
+		given()
 			.accept(ContentType.JSON)
 		.when()
 			.get()
