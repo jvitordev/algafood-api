3086059aee48c77b6a5f1b6a1513be33c4042736
Joao Vitor <joaovitorti07@gmail.com>
2023-03-05 13:29:28 -0300

    Aula 11.17. Mapeando para uma instância destino (e não um tipo) com ModelMapper

--- src/main/java/com/algaworks/algafood/api/assembier/RestauranteInputDisassembier.java
+++ src/main/java/com/algaworks/algafood/api/assembier/RestauranteInputDisassembier.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.api.model.input.RestauranteInput;
+import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Restaurante;
 
 @Component
@@ -16,5 +17,14 @@ public class RestauranteInputDisassembier {
     public Restaurante toDomainObject(RestauranteInput restauranteInput) {
         
 		return modelMapper.map(restauranteInput, Restaurante.class); 
+	}
+
+	public void copyToDomainModel(RestauranteInput restauranteInput, Restaurante restaurante) {
+
+		/* Para evitar: org.hibernate.HibernateException: identifier of an instance of
+		com.algaworks.algafood.domain.model.Cozinha was altered from 1 to 2 */
+		restaurante.setCozinha(new Cozinha());
+
+		modelMapper.map(restauranteInput, restaurante);
 	}
 }
--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.validation.Valid;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -77,15 +76,9 @@ public class RestauranteController {
         @PathVariable Long id,
         @Valid @RequestBody RestauranteInput restauranteInput) {
 
-        Restaurante restaurante = restauranteInputDisassembier.toDomainObject(restauranteInput);
-
         Restaurante restauranteAtual = cadastroRestaurante.buscarOuFalhar(id);
 
-        BeanUtils.copyProperties(
-            restaurante,
-            restauranteAtual,
-            "id", "formasPagamento", "endereco", "dataCadastro", "produtos"
-        );
+        restauranteInputDisassembier.copyToDomainModel(restauranteInput, restauranteAtual);
 
         try {
 
