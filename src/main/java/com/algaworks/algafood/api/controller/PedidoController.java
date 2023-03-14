4b07c23011ee8cfc09b78950ea40c994772aadd8
Joao Vitor <joaovitorti07@gmail.com>
2023-03-14 14:57:40 -0300

    Aula 13.2. Limitando os campos retornados pela API com @JsonFilter do Jackson

--- src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
@@ -2,13 +2,16 @@ package com.algaworks.algafood.api.controller;
 
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +27,8 @@ import com.algaworks.algafood.domain.model.Pedido;
 import com.algaworks.algafood.domain.model.Usuario;
 import com.algaworks.algafood.domain.repository.PedidoRepository;
 import com.algaworks.algafood.domain.service.EmissaoPedidoService;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 @RestController
 @RequestMapping("/pedidos")
@@ -45,12 +50,31 @@ public class PedidoController {
     PedidoResumoModelAssembler pedidoResumoModelAssembler;
 
     @GetMapping
-    public List<PedidoResumoModel> todos() {
+	public MappingJacksonValue listar(@RequestParam(required = false) String campos) {
+		List<Pedido> pedidos = pedidoRepository.findAll();
+		List<PedidoResumoModel> pedidosModel = pedidoResumoModelAssembler.toCollectionModel(pedidos);  
+		
+		MappingJacksonValue pedidosWrapper = new MappingJacksonValue(pedidosModel);
+		
+		SimpleFilterProvider filterProvider = new SimpleFilterProvider();
+		filterProvider.addFilter("pedidoFilter", SimpleBeanPropertyFilter.serializeAll());
+		
+		if (StringUtils.isNotBlank(campos)) {
+			filterProvider.addFilter("pedidoFilter", SimpleBeanPropertyFilter.filterOutAllExcept(campos.split(",")));
+		}
+		
+		pedidosWrapper.setFilters(filterProvider);
+		
+		return pedidosWrapper;
+	}
+    
+    // @GetMapping
+    // public List<PedidoResumoModel> todos() {
 
-        List<Pedido> pedidos = pedidoRepository.findAll();
+    //     List<Pedido> pedidos = pedidoRepository.findAll();
 
-        return pedidoResumoModelAssembler.toCollectionModel(pedidos);
-    }
+    //     return pedidoResumoModelAssembler.toCollectionModel(pedidos);
+    // }
 
     @GetMapping("/{codigoPedido}")
     public PedidoModel buscar(@PathVariable String codigoPedido) {
