1035ebd30c1a86eba782b0accad9d37427e3c019
Joao Vitor <joaovitorti07@gmail.com>
2023-03-14 20:39:28 -0300

    Aula 13.4. Implementando pesquisas simples na API

--- src/main/java/com/algaworks/algafood/domain/repository/PedidoRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/PedidoRepository.java
@@ -4,13 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import com.algaworks.algafood.domain.model.Pedido;
 
-// JpaSpecificationExecutor é responsável pelos métodos que executam objetos specification
-// estende a interface personalizada do Spring Data JPA, CustomJpaRepository
-@Repository
 public interface PedidoRepository extends CustomJpaRepository<Pedido, Long>{
 
     Optional<Pedido> findByCodigo(String codigo);
--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -5,13 +5,9 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import com.algaworks.algafood.domain.model.Restaurante;
 
-// JpaSpecificationExecutor é responsável pelos métodos que executam objetos specification
-// estende a interface personalizada do Spring Data JPA, CustomJpaRepository
-@Repository
 public interface RestauranteRepository extends CustomJpaRepository<Restaurante, Long>, RestauranteRepositoryQuery, JpaSpecificationExecutor<Restaurante> {
     
     @Query("from Restaurante r left join fetch r.cozinha")
