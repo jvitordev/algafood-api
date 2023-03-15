2b6b87b71aa1b211be422e6816d50bb31558fa6b
Joao Vitor <joaovitorti07@gmail.com>
2023-03-15 12:37:08 -0300

    Aula 13.8. Implementando paginação e ordenação em recursos de coleção da API

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -5,6 +5,10 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -41,8 +45,20 @@ public class CozinhaController {
     CozinhaInputDisassembier cozinhaInputDisassembier;
 
     @GetMapping
-    public List<CozinhaModel> todas() {
-        return cozinhaModelAssembier.toCollectionModel(cozinhaRepository.findAll());
+    public Page<CozinhaModel> todas(@PageableDefault(size = 2) Pageable pageable) {
+
+        Page<Cozinha> cozinhasPage = cozinhaRepository.findAll(pageable);
+
+        List<CozinhaModel> cozinhasModel = cozinhaModelAssembier
+        .toCollectionModel(cozinhasPage.getContent());
+
+        Page<CozinhaModel> cozinhaModelPage = new PageImpl<>(
+            cozinhasModel,
+            pageable,
+            cozinhasPage.getTotalElements()
+        );
+
+        return cozinhaModelPage;
     }
 
     @GetMapping("/{cozinhaId}")
