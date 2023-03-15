58c78e12212a1628aea0fd401d5e4c7a92279623
Joao Vitor <joaovitorti07@gmail.com>
2023-03-15 14:38:21 -0300

    Aula 13.9. Desafio: implementando paginação e ordenação de pedidos

--- src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
@@ -3,6 +3,10 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -47,10 +51,19 @@ public class PedidoController {
     PedidoResumoModelAssembler pedidoResumoModelAssembler;
 
     @GetMapping
-	public List<PedidoResumoModel> pesquisar(PedidoFilter filtro) {
-		List<Pedido> todosPedidos = pedidoRepository.findAll(PedidoSpecs.usandoFiltro(filtro));
+	public Page<PedidoResumoModel> pesquisar(PedidoFilter filtro, @PageableDefault(size = 10) Pageable pageable) {
+
+		Page<Pedido> pedidosPage = pedidoRepository.findAll(PedidoSpecs.usandoFiltro(filtro), pageable);
 		
-		return pedidoResumoModelAssembler.toCollectionModel(todosPedidos);
+        List<PedidoResumoModel> pedidoResumoModel = pedidoResumoModelAssembler.toCollectionModel(pedidosPage.getContent());
+
+        Page<PedidoResumoModel> pedidoResumoModelPage = new PageImpl<>(
+            pedidoResumoModel,
+            pageable,
+            pedidosPage.getTotalElements()
+        );
+
+        return pedidoResumoModelPage;
 	}
     
     @GetMapping("/{codigoPedido}")
