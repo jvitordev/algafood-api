510a7a4c4d084a70656f07f2c1868ed9270d593d
Joao Vitor <joaovitorti07@gmail.com>
2023-03-16 14:31:29 -0300

    Aulas 13.13 e 13.14 Implementando consulta com dados agregados de vendas diárias

--- src/main/java/com/algaworks/algafood/domain/filter/VendaDiariaFilter.java
+++ src/main/java/com/algaworks/algafood/domain/filter/VendaDiariaFilter.java
@@ -1,4 +1,4 @@
-package com.algaworks.algafood.domain.repository.filter;
+package com.algaworks.algafood.domain.filter;
 
 import java.time.OffsetDateTime;
 
@@ -8,17 +8,15 @@ import org.springframework.format.annotation.DateTimeFormat.ISO;
 import lombok.Getter;
 import lombok.Setter;
 
-@Setter
 @Getter
-public class PedidoFilter {
-
-	private Long clienteId;
-	private Long restauranteId;
+@Setter
+public class VendaDiariaFilter {
+    
+    private Long restauranteId;
 	
 	@DateTimeFormat(iso = ISO.DATE_TIME)
 	private OffsetDateTime dataCriacaoInicio;
 	
 	@DateTimeFormat(iso = ISO.DATE_TIME)
-	private OffsetDateTime dataCriacaoFim;
-	
+    private OffsetDateTime dataCriacaoFim;
 }
