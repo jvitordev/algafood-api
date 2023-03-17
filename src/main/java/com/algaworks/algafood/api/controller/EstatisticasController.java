cda53212fdd20feb62ec6ba0ea72a5d724c699b0
Joao Vitor <joaovitorti07@gmail.com>
2023-03-17 19:53:13 -0300

    Aula 13.19 e 13.20. Preenchendo um relatório JasperReports com JavaBeans e gerando bytes do PDF

--- src/main/java/com/algaworks/algafood/api/controller/EstatisticasController.java
+++ src/main/java/com/algaworks/algafood/api/controller/EstatisticasController.java
@@ -3,6 +3,9 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.algaworks.algafood.domain.filter.VendaDiariaFilter;
 import com.algaworks.algafood.domain.model.dto.VendaDiaria;
 import com.algaworks.algafood.domain.service.VendaQueryService;
+import com.algaworks.algafood.domain.service.VendaReportService;
 
 @RestController
 @RequestMapping(path = "/estatisticas")
@@ -18,6 +22,9 @@ public class EstatisticasController {
     
     @Autowired
     private VendaQueryService vendaQueryService;
+
+    @Autowired
+    private VendaReportService vendaReportService;
 
     @GetMapping("/vendas-diarias")
     public List<VendaDiaria> consuVendasDiarias(
@@ -27,4 +34,19 @@ public class EstatisticasController {
 
         return vendaQueryService.consultarVendasDiarias(filter, timeOffset);
     }
+
+    @GetMapping(path = "/vendas-diarias", produces = MediaType.APPLICATION_PDF_VALUE)
+	public ResponseEntity<byte[]> consultarVendasDiariasPdf(VendaDiariaFilter filtro,
+			@RequestParam(required = false, defaultValue = "+00:00") String timeOffset) {
+		
+		byte[] bytesPdf = vendaReportService.emitirVendasDiarias(filtro, timeOffset);
+		
+		var headers = new HttpHeaders();
+		headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=vendas-diarias.pdf");
+		
+		return ResponseEntity.ok()
+				.contentType(MediaType.APPLICATION_PDF)
+				.headers(headers)
+				.body(bytesPdf);
+	}
 }
