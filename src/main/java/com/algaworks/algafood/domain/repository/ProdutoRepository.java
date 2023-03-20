796cdf62a22fcea0ced8a37ccc9c7d95eb84e477
Joao Vitor <joaovitorti07@gmail.com>
2023-03-20 14:32:53 -0300

    Aula 14.6. Implementando serviço de cadastro de foto de produto

--- src/main/java/com/algaworks/algafood/domain/repository/ProdutoRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/ProdutoRepository.java
@@ -10,7 +10,7 @@ import org.springframework.data.repository.query.Param;
 import com.algaworks.algafood.domain.model.Produto;
 import com.algaworks.algafood.domain.model.Restaurante;
 
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+public interface ProdutoRepository extends JpaRepository<Produto, Long>, ProdutoRepositoryQueries {
 
     @Query("from Produto where restaurante.id = :restaurante and id = :produto")
     Optional<Produto> findById(@Param("restaurante") Long restauranteId, 
