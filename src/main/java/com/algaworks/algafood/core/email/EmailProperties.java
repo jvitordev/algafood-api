54b260585cc30f9d2ae9d28640cd80eec14bd245
Joao Vitor <joaovitorti07@gmail.com>
2023-03-24 15:20:46 -0300

    Aula 15.3. Implementando o serviço de infraestrutura de envio de e-mails com Spring

---
+++ src/main/java/com/algaworks/algafood/core/email/EmailProperties.java
@@ -0,0 +1,21 @@
+package com.algaworks.algafood.core.email;
+
+import javax.validation.constraints.NotBlank;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Validated
+@Getter
+@Setter
+@Component
+@ConfigurationProperties("algafood.email")
+public class EmailProperties {
+    
+    @NotBlank
+    private String remetente;
+}
