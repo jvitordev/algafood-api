c7134a2f82ffa892c916247f205825d38d3e2675
Joao Vitor <joaovitorti07@gmail.com>
2023-03-30 09:43:35 -0300

    Aula 17.2. Habilitando o cache com o cabeçalho Cache-Control e a diretiva max-age

--- src/main/java/com/algaworks/algafood/api/controller/FormaPagamentoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/FormaPagamentoController.java
@@ -1,11 +1,14 @@
 package com.algaworks.algafood.api.controller;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -40,9 +43,15 @@ public class FormaPagamentoController {
     private FormaPagamentoInputDisassembier formaPagamentoInputDisassembier;
 
     @GetMapping
-    public List<FormaPagamentoModel> todas() {
+    public ResponseEntity<List<FormaPagamentoModel>> todas() {
         
-        return formaPagamentoModelAssembier.toCollectionModel(formaPagamentoRepository.findAll());
+        List<FormaPagamento> todasFormasPagamentos = formaPagamentoRepository.findAll();
+
+        List<FormaPagamentoModel> formasPagamentosModel = formaPagamentoModelAssembier.toCollectionModel(todasFormasPagamentos);
+
+        return ResponseEntity.ok()
+            .cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS))
+            .body(formasPagamentosModel);
     }
 
     @GetMapping("/{id}")
