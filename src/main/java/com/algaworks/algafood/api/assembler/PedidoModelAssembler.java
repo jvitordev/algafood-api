cace55e86f2542ea27fc86bd9b639f6a8da22817
Joao Vitor <joaovitorti07@gmail.com>
2023-04-10 21:25:25 -0300

    Aula 19.18. Adicionando links com template variables

--- src/main/java/com/algaworks/algafood/api/assembler/PedidoModelAssembler.java
+++ src/main/java/com/algaworks/algafood/api/assembler/PedidoModelAssembler.java
@@ -5,6 +5,11 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.TemplateVariable;
+import org.springframework.hateoas.TemplateVariable.VariableType;
+import org.springframework.hateoas.TemplateVariables;
+import org.springframework.hateoas.UriTemplate;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.stereotype.Component;
 
@@ -32,7 +37,16 @@ public class PedidoModelAssembler extends RepresentationModelAssemblerSupport<Pe
 		
 		PedidoModel pedidoModel = createModelWithId(pedido.getCodigo(), pedido);
 
-        modelMapper.map(pedido, pedidoModel);
+		modelMapper.map(pedido, pedidoModel);
+
+		TemplateVariables pageVariables = new TemplateVariables(
+				new TemplateVariable("page", VariableType.REQUEST_PARAM),
+				new TemplateVariable("size", VariableType.REQUEST_PARAM),
+				new TemplateVariable("sort", VariableType.REQUEST_PARAM));
+		
+		String pedidosUrl = linkTo(PedidoController.class).toUri().toString();
+		
+		pedidoModel.add(Link.of(UriTemplate.of(pedidosUrl, pageVariables), "pedidos"));
 
 		pedidoModel.getRestaurante().add(linkTo(methodOn(RestauranteController.class)
 			.buscar(pedido.getRestaurante().getId())).withSelfRel());
@@ -50,9 +64,6 @@ public class PedidoModelAssembler extends RepresentationModelAssemblerSupport<Pe
 			item.add(linkTo(methodOn(RestauranteProdutoController.class)
 				.buscar(pedidoModel.getRestaurante().getId(), item.getProdutoId())).withSelfRel());
 		});
-
-		pedidoModel.add(linkTo(methodOn(PedidoController.class)
-			.pesquisar(null, null)).withRel("pedidos"));
 
 		return pedidoModel;
 	}
