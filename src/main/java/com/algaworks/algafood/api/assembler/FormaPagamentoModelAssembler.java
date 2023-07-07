09b2b3e3d6072904b13c47512ee2d06ec1e93019
Joao Vitor <joaovitorti07@gmail.com>
2023-07-06 21:27:15 -0300

    alterado "FormaPagamentoModelAssembler.java"

--- src/main/java/com/algaworks/algafood/api/assembler/FormaPagamentoModelAssembler.java
+++ src/main/java/com/algaworks/algafood/api/assembler/FormaPagamentoModelAssembler.java
@@ -7,18 +7,28 @@ import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.stereotype.Component;
 
+import com.algaworks.algafood.api.controller.FormaPagamentoController;
 import com.algaworks.algafood.api.model.FormaPagamentoModel;
 import com.algaworks.algafood.domain.model.FormaPagamento;
 
 @Component
-public class FormaPagamentoModelAssembler {
+public class FormaPagamentoModelAssembler extends RepresentationModelAssemblerSupport<FormaPagamento, FormaPagamentoModel> {
+
+	@Autowired
+	private ModelMapper modelMapper;
     
-    @Autowired
-    ModelMapper modelMapper;
+    public FormaPagamentoModelAssembler() {
+		super(FormaPagamentoController.class, FormaPagamentoModel.class);
+	}
     
     public FormaPagamentoModel toModel(FormaPagamento formaPagamento) {
+
+		FormaPagamentoModel formaPagamentoModel = createModelWithId(formaPagamento.getId(), formaPagamento);
+
+		modelMapper.map(formaPagamento, formaPagamentoModel);
 		
         return modelMapper.map(formaPagamento, FormaPagamentoModel.class);
 	}
